c66df389591671158d90c2a41de17ff6e5e54e5d
laizi <denghw3@gmail.com>
2018-01-20 16:04:33 +0800

    注册eureka集群

--- src/main/java/com/laizi/ProviderUserApplication.java
+++ src/main/java/com/laizi/ProviderUserApplication.java
@@ -3,8 +3,10 @@ package com.laizi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProviderUserApplication {
 
 	public static void main(String[] args) {
